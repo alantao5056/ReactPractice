b8d350136722a440cf4d3ce8450880c7b13205ff
alantao5056 <alantaolr@outlook.com>
2023-10-20 21:32:21 -0400

    fixed minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Title from './Title.jsx'
 import TodoCard from './TodoCard.jsx'
-import InputBox from './InputBox.jsx'
+import Input from './Input.jsx'
 
 export default function App() {
   return (
     <div id="main">
       <Title />
       <TodoCard text="hi"/>
-      <InputBox />
+      <Input />
     </div>
   )
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,29 +1,27 @@
-import { Card } from 'react-bootstrap';
+import TodoCard from './TodoCard';
 import { useState } from 'react';
-import './InputBox.css'
+import './Input.css'
 
 
 
-export default function InputBox() {
-  let val = ''
+export default function Input() {
   const [items, setItems] = useState([]);
   const submit = (e) => {
     e.preventDefault()
-    let input = document.getElementById('todo').value
+    let form = document.getElementById('todo')
+    let input = form.value
+    form.value = ''
 
-    const newCard = (<Card text={input}/>)
     setItems([
       ...items,
-      newCard
+      input
     ])
   }
   return (
-    <div className="input-box">
-      <>
-        {items.map(item => (
-          item
-        ))}
-      </>
+    <div className="input">
+      {items.map(item => (
+        <TodoCard text={item}></TodoCard>
+      ))}
       <form>
         <input type="text" id="todo" placeholder="What do you need to do?" />
         <button type="submit" onClick={(e)=>submit(e)}>Submit</button>
